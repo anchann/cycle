e8ff13b87a536e81ca7a16677598ebece2ddab2c
anchann <anchannnnnnnn@gmail.com>
2019-06-03 14:01:03 +0900

    Reimplemented

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
-# Cycle 🚲
+# PowerCycle 🚲
 
-Cycle through the .ts, .html, and .scss files of a component.
+Cycles through files in a directory. The files are globbed using a regex that can be configured.
+
+Used https://github.com/karsonbraaten/cycle as a starting point (though little of the original code remains).
 
 ## Features
 
-Keyboard shortcut to quickly switch between your .ts, .html, and .scss (or .css) files for a particular component.
+Keyboard shortcut to quickly switch between files in a directory (like logic/template/style files
+for a UI component).
 
 ## Extension Settings
 
 This extension contributes the following settings:
 
-* `extension.cycle`: cycle through files that only vary by extension
+* `powercycle.regex`: the regular expression used to glob files in the current file's directory, defaults to `*`
 
 ## Release Notes
 
 ### 1.0.0
 
-Initial release of cycle.
+Initial release of PowerCycle.
--- src/extension.ts
+++ src/extension.ts
@@ -1,48 +1,95 @@
 'use strict';
 import * as vscode from 'vscode';
+import { Uri } from 'vscode';
+
+type Option<T> = { value: T } | 'none';
+
+type Direction = 'forward' | 'backward';
 
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('extension.cycle', () => {
+    const command = (direction: Direction) => {
         var editor = vscode.window.activeTextEditor;
         if (!editor) {
             return;
         }
 
-        let fullPath = editor.document.fileName
-        cycle(fullPath);
-    });
+        cycle(editor.document.uri, direction);
+    }
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(vscode.commands.registerCommand('extension.powercycleForward',  () => command('forward')));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.powercycleBackward', () => command('backward')));
 }
 
-function cycle(filePath: string): void {
-    let fullFileName = filePath.substr(0, filePath.lastIndexOf('.'))
-    let extension = filePath.split('.').pop()
-    let fileName = fullFileName.substr(filePath.lastIndexOf('/') + 1)
+function getSortedUrisToCycleThrough(uri: Uri): Thenable<Uri[]> {
+    let directory = getDirectory(uri)
 
-    vscode.workspace.findFiles(`**/${fileName}.*`).then(uris => {
-        let extensions = [... new Set(uris.map(uri => uri.path.split('.').pop()))];
-        if (extensions.length < 2) {
-            vscode.window.showInformationMessage(`There exists no files with an extension differing from ${fullFileName}.${extension}.`);
-            return;
+    return findFiles(directory).then(uris => {
+        return uris.sort();
+    });
+}
+
+function getNextUri(currUri: Uri, direction: Direction): Thenable<Option<Uri>> {
+    return getSortedUrisToCycleThrough(currUri)
+    .then((uris: Uri[]): Option<Uri> => {
+        let currUriString = currUri.toString()
+        let currIndex = uris.findIndex(_ => _.toString() === currUriString)
+        if (currIndex === -1) {
+            return 'none'
         }
-        let index = extensions.indexOf(extension);
-        let newIndex = nextIndex(index, extensions);
-        let newExtension = extensions[newIndex];
-        
-        vscode.workspace.openTextDocument(`${fullFileName}.${newExtension}`).then(
-            textDocument => {
-                vscode.window.showTextDocument(textDocument);
-            },
-            error => {
-                vscode.window.showInformationMessage(`File ${fullFileName}.${newExtension} could not be found.`);
-            }
-        )
-    });
+
+        const increment = direction === 'forward' ? 1 : -1
+        const nextIndex = (currIndex + increment + uris.length) % uris.length;
+        return { value: uris[nextIndex] }
+    })
 }
 
-function nextIndex(currentIndex: number, items: any[]): number {
-    return (currentIndex + 1 === items.length) ? 0 : currentIndex + 1;
+function getDirectory(uri: Uri): string {
+    const parts = uri.path.split("/")
+    parts.pop()
+    return parts.join("/") + "/"
+}
+
+function adjustDirectoryForWorkspace(directory: string): string {
+    let workspaceAdjustedDirectory = directory
+    if (vscode.workspace.rootPath !== undefined) {
+        if (directory.startsWith(vscode.workspace.rootPath)) {
+            workspaceAdjustedDirectory = directory.slice(vscode.workspace.rootPath.length);
+
+            if (workspaceAdjustedDirectory.startsWith("/")) {
+                workspaceAdjustedDirectory = workspaceAdjustedDirectory.slice(1)
+            }
+        }
+    }
+    return workspaceAdjustedDirectory
+}
+
+function findFiles(directory: string): Thenable<Uri[]> {
+    const workspaceAdjustedDirectory = adjustDirectoryForWorkspace(directory)
+    const regex = vscode.workspace.getConfiguration("powercycle").regex
+    return vscode.workspace.findFiles(`${workspaceAdjustedDirectory}${regex}`)
+}
+
+function openUri(uri: Uri): void {
+    vscode.workspace.openTextDocument(uri).then(
+        textDocument => {
+            vscode.window.showTextDocument(textDocument);
+        },
+        error => {
+            vscode.window.showInformationMessage(`File ${uri} could not be found.`);
+        }
+    )
+}
+
+function cycle(currUri: Uri, direction: Direction): void {
+    getNextUri(currUri, direction)
+    .then(maybeNextUri => {
+        if (maybeNextUri === 'none') {
+            vscode.window.showInformationMessage(`No next file to cycle to.`);
+        }
+        else {
+            openUri(maybeNextUri.value)
+        }
+    })
 }
 
 export function deactivate() {}
